1094a7c0a2cdec19f33c549dd41638d155339b01
ThomasJazz <ttjazztt@gmail.com>
2018-04-30 22:19:16 -0700

    This will definitely not compile, but I've started the inner class LooneyTune that extends Thread (This is apparently how we're supposed to multi thread in Java)

--- src/JTune.java
+++ src/JTune.java
@@ -1,8 +1,33 @@
 /**
- * Project Description goes here
+ * I think rather than trying to copy over his c++ code we should just try
+ * to come up with our own java solution that will produce the example output
+ * and then work from there on the rest of the project.
  */
 import java.lang.Thread;
+private int N = 3, TWEETY = 0, MUTTLEY = 1, sleep = 1000;
 
+// In Java we use multithreading by extending "Thread" class
+class LooneyTune extends Thread {
+  // pthread_t thread_id; we need to come up with a java solution for this
+  int thread_num, condition, position, copy_FINISH_LINE, copy_COUNT_GLOBAL;
+  String name;
+
+  public void run() {
+    try {
+      System.out.println("Thread " + Thread.currentThread().getID() +
+      " is running");
+    } catch (Exception e) {
+      System.out.println(e.getStackTrace());
+    }
+  }
+}
+
+class SHARED {
+  int CONDITION_TOON, WHOLE_CYCLE, COUNT_GLOBAL, FINISH_LINE;
+  int[] FROZEN = new int[2];
+  int[] TOON_POSITION = new int[2];
+  char[] winner = new char[15];
+}
 public class JTune {
     private void print_board_vertical(int toon_pos[]) {
         char toon_letter[2]={'T','M'};
@@ -44,8 +69,19 @@ public class JTune {
     }
 
     private void toon_signal(ThreadData *toon) {
-
+      pthread
     }
+
+    public void run_API(Thread thread) {
+      LooneyTune[] characters = new LooneyTune[3];
+      setup_time_seed();
+
+      while (!characters.copy_FINISH_LINE) {
+        toon_signal(characters);
+        Sleep *= 2;
+      }
+    }
+
     public static void main(String[] args){
 
     }
