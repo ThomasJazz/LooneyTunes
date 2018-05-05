49ecb78274ce99a8f9350211bd1748e747b751c2
CwuAl <37880423+CwuAl@users.noreply.github.com>
2018-05-05 05:09:56 -0700

    Deleted SHARED.java

--- src/SHARED.java
+++
@@ -1,7 +0,0 @@
-package cs380looneytoons;
-
-public class SHARED {
-    // not sure whether we need to use this or not.
-    // was getting used to threading last night so I just moved all the variables
-    // to ThreadData fields
-}
